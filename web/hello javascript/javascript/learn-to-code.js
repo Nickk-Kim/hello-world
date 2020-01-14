ec64dd44b7ce60d86f91741d9a0830ca97487c22
Nickk-Kim <smilenick0717@gmail.com>
2020-01-14 20:05:17 +0900

    Javascript Basic

---
+++ web/hello javascript/javascript/learn-to-code.js
@@ -0,0 +1,20 @@
+var name = "Nick";
+var age = 31;
+
+var message = "Hi, my name is " + name + " and I am " + age + " years old!";
+
+var dateofBirth = "17-07-88"
+var loginWelcomeMessage = "Welcome, " + name + ". ";
+console.log(message);
+
+
+var balances = [50.45, 4000.12, -300.50];
+
+var studentNames = ["Timmy", "Janessa", "Arune"]
+
+
+if (1 == 1) {
+    console.log("We should see this!");
+}
+
+
