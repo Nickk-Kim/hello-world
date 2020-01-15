d4a1e1f8ff34ad62b314ff1630016526cf8792b0
Nickk-Kim <smilenick0717@gmail.com>
2020-01-15 19:47:46 +0900

    added function and object features

--- web/hello javascript/javascript/learn-to-code.js
+++ web/hello javascript/javascript/learn-to-code.js
@@ -18,3 +18,103 @@ if (1 == 1) {
 }
 
 
+var students = ["Timmy", "Janessa", "Arun"];
+var naughtyList = [];
+
+naughtyList.push(students[0]);
+
+var index = naughtyList.indexOf("Timmy");
+
+console.log(naughtyList);
+console.log(index);
+
+
+if(index > -1) {
+    naughtyList.splice(index, 1);
+}
+
+console.log(naughtyList);
+
+var someVal = 10;
+for (var x = 0; x < someVal; x++) {
+    console.log(x);
+}
+
+
+var length1 = 15;
+var width1 = 10;
+var area1 = length1 * width1;
+
+var length2 = 12;
+var width2 = 14;
+var area2 = length2 * width2;
+
+function area(length, width) {
+    return length * width;
+}
+
+
+console.log(area1);
+console.log(area2);
+
+var area10 = area(10,15);
+console.log(area10);
+
+
+var student = {
+    firstName: "John",
+    lastName: "Parker",
+    age: 7
+}
+
+var student1 = new Object();
+student1.firstName = "John";
+student1.lastName = "Parker";
+student1.age = 7;
+
+var student2 = {};
+student2.firstName = "Zack";
+student2.lastName = "Bobo";
+student2.age = 5;
+
+
+console.log(student);
+console.log(student.firstName);
+console.log(student.lastName);
+console.log(student.age);
+
+console.log(student1);
+console.log(student1.firstName);
+console.log(student1.lastName);
+console.log(student1.age);
+
+console.log(student2);
+console.log(student2.firstName);
+console.log(student2.lastName);
+console.log(student2.age);
+
+var student0 = {
+    firstName: "Jayne",
+    lastName: "Looo",
+    age: 8,
+    greeting: function() {
+        return "Hi, I'm " + this.firstName + " and I'm " + this.age + " years old. ";
+    }
+}
+
+console.log(student0.greeting());
+
+
+function Student(first, last, age) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    
+}
+
+var s1 = new Student("Jenny", "Lage", 5);
+console.log(s1);
+console.log(s1.firstName);
+
+
+students.push
